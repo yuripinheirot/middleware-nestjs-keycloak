68324bf6237a2b0e8f44c0d51eb0e7bdcfe4cce6
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-10-28 14:53:15 -0400

    refactor: add filter null sprites

--- src/modules/pokemon/mappers/pokemon.mapper.ts
+++ src/modules/pokemon/mappers/pokemon.mapper.ts
@@ -1,21 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { PokemonApiType } from 'src/shared/types/pokemonApi.type';
 import { PokemonType } from '../types/pokemon.type';
 import { PokemonSpecieApiDto } from 'src/shared/services/pokeapi/dto/pokemonSpecieApi.response.dto';
 
+type SpritesType = PokemonType['sprites'];
 export class PokemonMapper {
   static buildPokemon(data: {
     pokemonApiData: PokemonApiType;
     pokemonSpecieApiData: PokemonSpecieApiDto;
   }): PokemonType {
-    const entries = data.pokemonSpecieApiData.flavor_text_entries;
+    const { flavor_text_entries: entries } = data.pokemonSpecieApiData;
+    const { sprites } = data.pokemonApiData;
+
     const entryEnglish = entries.find((entry) => entry.language.name === 'en');
     const description = entryEnglish.flavor_text || entries[0].flavor_text;
+
+    const spritesFiltered = Object.entries(sprites).reduce(
+      (acc, [key, value]) => {
+        if (value) {
+          acc[key] = value;
+        }
+        return acc;
+      },
+      {},
+    );
 
     return {
       id: data.pokemonApiData.id,
       name: data.pokemonApiData.name,
       description: description.replace(/[\n\f]/g, ' '),
-      sprites: data.pokemonApiData.sprites,
+      sprites: spritesFiltered as SpritesType,
     };
   }
 }
--- src/shared/types/pokemonApi.type.ts
+++ src/shared/types/pokemonApi.type.ts
@@ -1,12 +1,12 @@
 export type PokemonSpritesApiType = {
-  back_default: string;
-  back_female: string;
-  back_shiny: string;
-  back_shiny_female: string;
-  front_default: string;
-  front_female: string;
-  front_shiny: string;
-  front_shiny_female: string;
+  back_default?: string;
+  back_female?: string;
+  back_shiny?: string;
+  back_shiny_female?: string;
+  front_default?: string;
+  front_female?: string;
+  front_shiny?: string;
+  front_shiny_female?: string;
 };
 
 export type PokemonApiType = {
