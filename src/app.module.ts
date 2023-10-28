80b59f87076d7ef91667ff1a5e7b460ddc4159cc
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-10-27 22:43:32 -0400

    chore: add caching with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { redisStore } from 'cache-manager-redis-store';
 import { StatusModule } from './modules/status/status.module';
 import { ConfigModule } from '@nestjs/config';
 import { PokemonModule } from './modules/pokemon/pokemon.module';
 import { SharedModule } from './shared/shared.module';
+import type { RedisClientOptions } from 'redis';
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
@@ -11,6 +14,16 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    CacheModule.register<RedisClientOptions>({
+      ttl: +process.env.TTL_CACHE_IN_MS,
+      max: 3,
+      isGlobal: true,
+      store: redisStore as unknown as CacheStore,
+      socket: {
+        port: 6379,
+        host: 'redis',
+      },
     }),
   ],
   controllers: [],
--- src/modules/pokemon/pokemon.controller.ts
+++ src/modules/pokemon/pokemon.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  Param,
+  UseInterceptors,
+} from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CustomLoggerService } from 'src/shared/logger/logger.service';
 import { AxiosErrorsInterceptor } from 'src/shared/interceptors/express.interceptor';
+import { CACHE_MANAGER, CacheInterceptor } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Controller('pokemon')
 @UseInterceptors(AxiosErrorsInterceptor)
+@UseInterceptors(CacheInterceptor)
 export class PokemonController {
   constructor(
     private readonly logger: CustomLoggerService,
     private readonly pokemonService: PokemonService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {
     this.logger.setPrefix(`@Controller('pokemon')`);
   }
