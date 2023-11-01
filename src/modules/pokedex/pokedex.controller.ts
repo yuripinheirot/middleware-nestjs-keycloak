7f6410af470a2a68a847836b60ffc915d5bf6e38
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-10-31 22:37:01 -0400

    feat: add postgres on docker

--- src/modules/pokedex/pokedex.controller.ts
+++ src/modules/pokedex/pokedex.controller.ts
@@ -1,9 +1,7 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { PokedexService } from './pokedex.service';
-import { AuthGuard } from 'src/shared/guards/Authentication.guard';
 
 @Controller('pokedex')
-@UseGuards(AuthGuard)
 export class PokedexController {
   constructor(private readonly pokedexService: PokedexService) {}
 
